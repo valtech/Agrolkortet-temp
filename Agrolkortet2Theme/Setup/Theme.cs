d71a7c0ddec582ab19c9caae0f7912ff478c4451
Davval <david.samuelsson@valtech.se>
2020-02-19 15:21:34 +0100

    Removed old teaser styles

--- Agrolkortet2Theme/Setup/Theme.cs
+++ Agrolkortet2Theme/Setup/Theme.cs
@@ -14,9 +14,7 @@ namespace Lantmannen.Agrolkortet2Theme.Setup
             get
             {
 	            // Default "Standard" styles (All modules should have one, use string.Empty for no class)
-                yield return new ThemeStyle("CalloutBlock", "Image (67%) and text (33%)", "callout--67-33", true);
-                yield return new ThemeStyle("CalloutBlock", "Image and caption 33%", "callout--33");
-                yield return new ThemeStyle("CalloutBlock", "Image and caption 100%", "callout--100");
+                yield return new ThemeStyle("CalloutBlock", "Standard", "string.Empty", true);
 
                 yield return new ThemeStyle("LogoBlock", "Standard", string.Empty, true);
 
