37a77d98176015579b500e905e994261ead79abd
davval <42937369+davval@users.noreply.github.com>
2020-02-21 15:17:01 +0100

    Feature/33 article page (#3)
    * Added banner for article page
    
    * Added pure js cookie script
    
    * Added style fix for article page blue box
    
    * Added pure js responsiveTables + style for table
    
    * Removed full width article top image
    
    * Added multicolor to all titles
    
    * Changed array method in responsive tables

---
+++ Agrolkortet2Theme/src/js/apps/ResponsiveTables.js
@@ -0,0 +1,40 @@
+window.Agrolkortet2Theme = window.Agrolkortet2Theme || {};
+
+(function (app) {
+    'use strict';
+
+    app.ResponsiveTables = function() {
+        const tables = document.querySelectorAll('table');
+
+        const wrap = function(element, wrapper) {
+            wrapper.appendChild(element);
+        }
+
+        const wrapAndReplace = function(element) {
+            const copy = element.cloneNode(true);
+
+            const innerWrapper = document.createElement('div');
+            innerWrapper.classList.add("table-wrapper--inner")
+
+            const outerWrapper = document.createElement('div');
+            outerWrapper.classList.add("table-wrapper")
+
+            if (element.offsetWidth > element.parentNode.offsetWidth) {
+                outerWrapper.classList.add("fade-right")
+            }
+
+            wrap(copy, innerWrapper);
+            wrap(innerWrapper, outerWrapper);
+
+            element.outerHTML = outerWrapper.outerHTML
+        }
+
+        const init = function() {
+            Array.from(tables).forEach(table => {
+                wrapAndReplace(table)
+            })
+        };
+
+        init();
+    };
+}(window.Agrolkortet2Theme));
