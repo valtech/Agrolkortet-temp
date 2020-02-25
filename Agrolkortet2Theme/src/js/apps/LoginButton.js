950b08b8db7b017b7d19e60c018f7d1a7540783e
Johannes Almroth <johannes.almroth@gmail.com>
2020-02-25 08:41:11 +0100

    Base setup for login box

---
+++ Agrolkortet2Theme/src/js/apps/LoginButton.js
@@ -0,0 +1,12 @@
+﻿window.Agrolkortet2Theme = window.Agrolkortet2Theme || {};
+
+(function (app) {
+	var loginArea = document.querySelector(".header-login");
+	var button = header.querySelector(".login-button");
+
+	button.addEventListener("click", function (e) {
+		e.preventDefault();
+		loginArea.classList.toggle("login-open");
+	}, false);
+
+})(window.Agrolkortet2Theme);
